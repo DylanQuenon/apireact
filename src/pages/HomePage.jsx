3481aa948207ceb264b80fcaf8bea3b0809223ab
Quenon Dylan  Ghislain <quenond@classes.local>
2024-01-16 09:26:58 +0100

    cours 1 installation

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+const HomePage = (props) => {
+    return ( 
+        <div className="jumbotron">
+            <h1 className="display-3">Hello World</h1>
+            <p className="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic architecto quis eos id deserunt magnam.</p>
+            <hr className="my-4" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, rem?</p>
+            <p className="lead">
+                <a href="#" className="btn btn-primary btn-lg">Learn More</a>
+            </p>
+        </div>
+     );
+}
+ 
+export default HomePage;
